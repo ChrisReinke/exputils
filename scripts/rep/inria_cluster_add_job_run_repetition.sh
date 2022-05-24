9c3a62de664c9ae3114819d5d085037511d5b50a
Chris Reinke <chris.reinke@inria.fr>
2022-05-24 17:34:56 +0200

    version 0.2.22  - bugfix: inria_cluster_add_job_run_repetition.sh file was also starting already finished jobs

--- scripts/rep/inria_cluster_add_job_run_repetition.sh
+++ scripts/rep/inria_cluster_add_job_run_repetition.sh
@@ -5,21 +5,39 @@
 ##########################
 # Parameters
 WALLTIME=$EU_PRJ_INRIA_CLUSTER_DEFAULT_WALLTIME
-CLUSTER=$EU_PRJ_INRIA_CLUSTER_DEFAULT_CLUSTER
+CLUSTER=$EU_CLUSTER
 RUN_REPETITION_SCRIPT=$EU_PRJ_DEFAULT_RUN_REPETITION_SCRIPT
-
+ACCESSMACHINE=$EU_PRJ_INRIA_CLUSTER_ACCESS_MACHINE
+MACHINE=$EU_MACHINE
 ##########################
 # Execution
 
-while getopts ":w:c" arg; do
+while getopts ":m:w:c" arg; do
   case $arg in
+    m) MACHINE=$OPTARG;;
     w) WALLTIME=$OPTARG;;
     c) CLUSTER=$OPTARG;;
   esac
 done
 
+# if machine is given, then create string 'AND <machine>' to add to the command
+ADDMACHINE=
+if [ -n "$MACHINE" ]; then
+    ADDMACHINE="AND host='$MACHINE'"
+fi
+
+# path to experiments on the local host
+LOCAL_EXPERIMENTS_DIR=$EU_PRJ_LOCAL_EXPERIMENTS_PATH
+# path to the experiments directory on the target host
+EXTERNAL_EXPERIMENTS_DIR=$EU_PRJ_INRIA_EXPERIMENTS_PATH
+
+# replace the path to the experiments on local host with the one at the target host
+LOCAL_SCRIPT_PATH=`pwd`
+EXTERNAL_PATH=`echo $LOCAL_SCRIPT_PATH | sed "s+${LOCAL_EXPERIMENTS_DIR}+${EXTERNAL_EXPERIMENTS_DIR}+g"`
+
+
 # identify if the repetition needs to run to only create a job if necessary
-NUM_REPETITIONS_TO_EXECUTE=`eu_gricad_python -c "
+NUM_REPETITIONS_TO_EXECUTE=`python -c "
 import exputils
 
 n_scripts = exputils.manage.get_number_of_scripts_to_execute(
@@ -29,25 +47,11 @@ n_scripts = exputils.manage.get_number_of_scripts_to_execute(
 print(n_scripts)"`
 
 if [ $NUM_REPETITIONS_TO_EXECUTE -gt 0 ]; then
-
-  # Create a script that runs the job. It has two features:
-  # 1) identify if the repetition still has to run
-  # 2) activate the conda environment
-  echo "NUM_REPETITIONS_TO_EXECUTE=\`eu_inria_singularity python -c \"
-import exputils
-
-n_scripts = exputils.manage.get_number_of_scripts_to_execute(
-  directory='.',
-  start_scripts='$RUN_REPETITION_SCRIPT')
-
-print(n_scripts)\"\`
-
-if [ \$NUM_REPETITIONS_TO_EXECUTE -gt 0 ]; then
-  eu_inria_singularity ./\$EU_PRJ_DEFAULT_RUN_REPETITION_SCRIPT
-fi" > oar_job_run_repetition.sh
-  chmod +x oar_job_run_repetition.sh
-
   echo "Register current repetition as a job ..."
-  oarsub -l /host=1/gpudevice=1,walltime=$WALLTIME -p "cluster='$CLUSTER'" -t besteffort --stdout inria_cluster_run_repetition.out.log --stderr inria_cluster_run_repetition.err.log ./oar_job_run_repetition.sh
+  ssh $ACCESSMACHINE "source eu_activate $EU_ACTIVE_PRJ;
+    cd ${EXTERNAL_PATH};
+    echo \"eu_inria_singularity ./\$EU_PRJ_DEFAULT_RUN_REPETITION_SCRIPT\" > oar_job_run_repetition.sh;
+    chmod +x oar_job_run_repetition.sh;
+    oarsub -l /host=1/gpudevice=1,walltime=$WALLTIME -p \"cluster='$CLUSTER' $ADDMACHINE\" -t besteffort --stdout inria_cluster_run_repetition.out.log --stderr inria_cluster_run_repetition.err.log ./oar_job_run_repetition.sh"
   echo "Finished."
 fi
